1ed46612fce3d3cf3aaf5694d4fbd063e8199d7d
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2025-01-18 22:27:34 +0330

    :zap: modify formater :)

--- index.ts
+++ index.ts
@@ -3,13 +3,36 @@ import init, { format_rust_code } from "./pkg/rustfmt_wasm.js";
 async function run() {
   await init();
 
-  const code = `
+  const code = `use std::io;
+  use std::io::Write;
+  use std::io::Read;
+  use std::io::BufReader;
+  use std::io::BufRead;
+  use std::io::BufWriter;
+  use std::io::Write;
+  use std::io::Read;
+    
+
+
+  
+
   struct  Foo {
   bar: i32,
   baz: i32,
   }
+  static MY: u32 = 10;
+
   fn main() {
   println!("Hello, world!");
+  let x = 1 +    1;
+
+  match x {
+  1 => println!("one"),
+  2 => println!("two"),
+  3 => println!("three"),
+  _ => println!("anything"),
+  
+  }
               }
   `;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,69 @@
-use wasm_bindgen::prelude::*;
 use prettyplease::unparse;
 use syn::File;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn format_rust_code(code: &str) -> Result<String, JsValue> {
     // Parse the input code into a syntax tree
-    let syntax_tree: File = syn::parse_str(code)
-        .map_err(|e| JsValue::from_str(&format!("Parsing error: {}", e)))?;
+    let syntax_tree: File =
+        syn::parse_str(code).map_err(|e| JsValue::from_str(&format!("Parsing error: {}", e)))?;
 
     // Format the code using prettyplease
-    let formatted_code = unparse(&syntax_tree);
+    let formatted = unparse(&syntax_tree);
 
-    Ok(formatted_code)
+    let mut result = formatted;
+
+    result = format_imports_and_modules(&result);
+
+    result = add_spaces_after_blocks_expr(&result);
+
+    Ok(result)
+}
+
+fn add_spaces_after_blocks_expr(code: &str) -> String {
+    let mut result = String::new();
+    let mut lines = code.lines().peekable();
+
+    while let Some(line) = lines.next() {
+        result.push_str(line);
+        result.push('\n');
+
+        // Check if the current line ends with a closing brace `}`
+        if line.trim() == "}" {
+            // Add an extra newline after the closing brace
+            result.push('\n');
+        }
+    }
+
+    result
+}
+
+fn format_imports_and_modules(code: &str) -> String {
+    let mut result = String::new();
+    let mut imports = Vec::new();
+    let mut other_code = Vec::new();
+
+    for line in code.lines() {
+        if line.trim().starts_with("use ") || line.trim().starts_with("mod ") {
+            imports.push(line.trim());
+        } else {
+            other_code.push(line);
+        }
+    }
+
+    // Sort and format imports
+    imports.sort();
+    for import in imports {
+        result.push_str(&format!("{}\n", import));
+    }
+
+    result.push_str(&format!("\n\n"));
+
+    // Add the rest of the code
+    for line in other_code {
+        result.push_str(line);
+        result.push('\n');
+    }
+
+    result
 }
